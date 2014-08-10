548fb2672d3f1518bb9ae72cdaa1d1fe8b46e59f
r4start <r4start@gmail.com>
2014-08-10 23:45:24 +0400

    Two simple functions.

--- led/native-playground/ws2801_strip.cpp
+++ led/native-playground/ws2801_strip.cpp
@@ -3,23 +3,60 @@
 using std::size_t;
 using std::vector;
 using std::string;
+using std::fstream;
+using std::runtime_error;
 
 using colour_code = std::array<uint8_t, 3>;
 
+static constexpr colour_code WHITE{{0xff, 0xff, 0xff}};
 static constexpr colour_code BLACK{{0x00, 0x00, 0x00}};
 static constexpr colour_code BLUE_VIOLET{{0x8a, 0x2b, 0xe2}};
 
 
 namespace led { namespace chip {
 
-ws2801_chip::ws2801_chip(const string &device_name) {}
+ws2801_chip::ws2801_chip(const string &device_name)
+  : device_{device_name} {
+
+  if (!device_.is_open()) {
+    throw runtime_error{"Unable to open device!"};
+  }
+}
+
+ws2801_chip::~ws2801_chip() {
+  device_.close();
+}
 
 void ws2801_chip::turn_leds_on(size_t led_count) {
-  ;
+  if (!led_count) {
+    throw runtime_error{"Bad led count!"};
+  }
+
+  vector<uint8_t> led_array(led_count);
+  for (size_t i = 0; i < led_count; ++i) {
+    led_array[i] = WHITE[0];
+    led_array[i + 1] = WHITE[1];
+    led_array[i + 2] = WHITE[2];
+  }
+
+  device_.write(reinterpret_cast<char *>(led_array.data()), led_count);
+  device_.flush();
 }
 
 void ws2801_chip::turn_leds_off(size_t led_count) {
-  ;
+  if (!led_count) {
+    throw runtime_error{"Bad led count!"};
+  }
+
+  vector<uint8_t> led_array(led_count);
+  for (size_t i = 0; i < led_count; ++i) {
+    led_array[i] = BLACK[0];
+    led_array[i + 1] = BLACK[1];
+    led_array[i + 2] = BLACK[2];
+  }
+
+  device_.write(reinterpret_cast<char *>(led_array.data()), led_count);
+  device_.flush();
 }
 
 void ws2801_chip::set_strip_colour(const vector<uint8_t> &pixels) {
--- led/native-playground/ws2801_strip.h
+++ led/native-playground/ws2801_strip.h
@@ -1,6 +1,8 @@
 #ifndef WS2801_STRIP_H__
 #define WS2801_STRIP_H__
 
+#include <fstream>
+
 #include "colour.h"
 
 namespace led { namespace chip {
@@ -8,7 +10,7 @@ namespace led { namespace chip {
 class ws2801_chip : public virtual colour_strip {
 public:
   explicit ws2801_chip(const std::string &device_name);
-  ~ws2801_chip() override = default;
+  ~ws2801_chip() override;
 
   ws2801_chip(const ws2801_chip &) = delete;
   ws2801_chip &operator=(const ws2801_chip &) = delete;
@@ -16,6 +18,9 @@ public:
   void turn_leds_on(std::size_t led_count) override;
   void turn_leds_off(std::size_t led_count) override;
   void set_strip_colour(const std::vector<uint8_t> &pixels) override;
+
+private:
+  std::fstream device_;
 };
 
 }
